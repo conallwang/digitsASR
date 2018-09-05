03ded89ad786af65ae44ed2c33bf4d55a4e6b314
Johnson <17319255967@163.com>
2018-09-05 15:37:17 +0800

    first commit

---
+++ digits/run.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+. ./path.sh || exit 1
+. ./cmd.sh || exit 1
+
+nj=4
+lm_order=1
+
+. utils/parse_options.sh || exit 1
+[[ $# -ge 1 ]] && { echo "Wrong arguments!"; exit 1; }
+
+
+# Removing previously created data (from last run.sh execution)
+rm -rf exp mfcc data/train/spk2utt data/train/cmvn.scp data/train/feats.scp \
+      data/train/split1 data/test/spk2utt data/test/cmvn.scp data/test/feats.scp \
+      data/test/split1 data/local/lang data/lang data/local/tmp \
+      data/local/dict/lexiconp.txt
+
+
+echo
+echo "===== PREPARING ACOUSTIC DATA ====="
+echo
+
+# Making spk2utt files
+utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+
+echo
+echo "===== FEATURES EXTRACTION ====="
+echo
+
+# Making feats.scp files
+mfccdir=mfcc
+# Uncomment and modify arguments in scripts below if you have any problems with data sorting
+# utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
+# utils/validate_data_dir.sh data/test
+# utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
+# utils/fix_data_dir.sh data/test
+
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train \
+                        exp/make_mfcc/train $mfccdir
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/test \
+                        exp/make_mfcc/test $mfccdir
+
+# Making cmvn.scp files
+steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
+steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
+
+echo
+echo "===== PREPARING LANGUAGE DATA ====="
+echo
+# Preparing language data
+utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
+
+echo
+echo "===== LANGUAGE MODEL CREATION ====="
+echo "===== MAKING lm.arpa ====="
+echo
+
+loc=`which ngram-count`;
+if [ -z $loc ]; then
+    if uname -a | grep 64 >/dev/null; then
+        sdir=$KALDI_ROOT/tools/srilm/bin/i686-m64
+    else
+        sdir=$KALDI_ROOT/tools/srilm/bin/i686
+    fi
+    if [ -f $sdir/ngram-count ]; then
+        echo "Using SRILM language modelling tool from $sdir"
+        export PATH=$PATH:$sdir
+    else
+        echo "SRILM toolkit is probably not installed. Instructions: tools/install_srilm.sh"
+        exit 1
+    fi
+fi
+
+local=data/local
+mkdir $local/tmp
+ngram-count -order $lm_order -write-vocab $local/tmp/vocab-full.txt \
+            -wbdiscount -text $local/corpus.txt -lm $local/tmp/lm.arpa
+
+echo
+echo "===== MAKING G.fst ====="
+echo
+ 
+lang=data/lang
+arpa2fst --disambig-symbol=#0 --read-symbol-table=$lang/words.txt \
+                          $local/tmp/lm.arpa $lang/G.fst
+ 
+echo
+echo "===== MONO TRAINING ====="
+echo
+ 
+steps/train_mono.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono  || exit 1
+ 
+echo
+echo "===== MONO DECODING ====="
+echo
+ 
+utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
+                exp/mono/graph data/test exp/mono/decode
+local/score.sh data/test data/lang exp/mono/decode/
+
+echo
+echo "===== MONO ALIGNMENT ====="
+echo
+
+steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali || exit 1
+ 
+echo
+echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo
+
+steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1
+ 
+echo
+echo "===== TRI1 (first triphone pass) DECODING ====="
+echo
+ 
+utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
+                exp/tri1/graph data/test exp/tri1/decode
+local/score.sh data/test data/lang exp/tri1/decode
+
+echo
+echo "===== run.sh script is finished ====="
+echo
